147563a998af459a0c6f6c468a81176e423e11fc
AlanJimenez353 <ajimenez@technisys.com>
2022-05-14 21:05:05 -0300

    agrego un sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Primer commit");
         System.out.println("segundo");
+        System.out.println("asd");
 
     }
 }
