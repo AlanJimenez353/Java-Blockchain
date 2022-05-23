1f49b6a5424f223927c0143fd39279a23196a063
Nicolas Ale <NicolasAle97@hotmail.com>
2022-05-23 17:41:17 -0300

    Inicio del TP

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Primer commit");
-        System.out.println("segundo");
-        System.out.println("asd");
+
 
     }
 }
+
+
+/*
+Un menor de edad no puede crear una cuenta.
+No se puede buscar por nombre ya que pueden existir varios Usuarios con el mismo nombre.
+Cada Usuario tiene un ID , puede ser autoincremental.
+ */
