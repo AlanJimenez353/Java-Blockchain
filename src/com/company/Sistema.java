3e59c33cdcbbb15d277b9428130ccd42ff3137d6
Nicolas Ale <NicolasAle97@hotmail.com>
2022-06-06 22:39:30 -0300

    Agrego Wallet, validaciones de login

--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -2,10 +2,8 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -198,31 +196,78 @@ public class Sistema {
 
 
     public String ingresarEmail() {
-        String emailNuevo;
+        String emailNuevo="Error";
         boolean inspector;
         Scanner scan = new Scanner(System.in);
 
-        do {
-            System.out.println("Email: ");
-            emailNuevo = scan.nextLine();
-            inspector = validEmail(emailNuevo);
-        } while (!inspector);
+        try {
+            do {
+                System.out.println("Email: ");
+                emailNuevo = scan.nextLine();
+                inspector = validEmail(emailNuevo);
+            } while (!inspector);
+        }catch (Exception e)
+        {
+            System.out.println("Error fatal en el ingreso del email.");
+        }
 
         return emailNuevo;
     }
 
     public String ingresarPassword() {
-        String password;
+        String password="Error";
         boolean inspector;
         Scanner scan = new Scanner(System.in);
 
-        do {
-            System.out.println(" Password : ");
-            password = scan.nextLine();
-            inspector = validPassword(password);
-        } while (!inspector);
+        try {
+            do {
+                System.out.println(" Password : ");
+                password = scan.nextLine();
+                inspector = validPassword(password);
+            } while (!inspector);
+        }catch (Exception e)
+        {
+            System.out.println("Error fatal en el ingreso de la password.");
+        }
 
         return password;
+    }
+
+    public String ingresarDNI() {
+        String dni="Error";
+        boolean inspector;
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            do {
+                System.out.println(" DNI : ");
+                dni = scan.nextLine();
+                inspector = validDni(dni);
+            } while (!inspector);
+        }catch (Exception e)
+        {
+            System.out.println("Error fatal en el ingreso del DNI.");
+        }
+
+        return dni;
+    }
+
+    public String ingresarUserName() {
+        String userName="Error";
+        boolean inspector;
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            do {
+                System.out.println(" Username : ");
+                userName = scan.nextLine();
+                inspector = validUsername(userName);
+            } while (!inspector);
+        }catch (Exception e)
+        {
+            System.out.println("Error fatal al ingresar el nombre de usuario.");
+        }
+        return userName;
     }
 
     private boolean validPassword(String password) {
@@ -234,46 +279,194 @@ public class Sistema {
         int lowercaseCounter = 0;
         int digitCounter = 0;
 
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            if (Character.isUpperCase(c))
-                uppercaseCounter++;
-            else if (Character.isLowerCase(c))
-                lowercaseCounter++;
-            else if (Character.isDigit(c))
-                digitCounter++;
-        }
+        if (password != null) {
+            for (int i = 0; i < password.length(); i++) {
+                char c = password.charAt(i);
+                if (Character.isUpperCase(c))
+                    uppercaseCounter++;
+                else if (Character.isLowerCase(c))
+                    lowercaseCounter++;
+                else if (Character.isDigit(c))
+                    digitCounter++;
+            }
 
-        if (password.length() >= MAX && uppercaseCounter >= MIN_Uppercase && lowercaseCounter >= MIN_Lowercase && digitCounter >= NUM_Digits) {
-            System.out.println("Valid Password");
-            return true;
+            if (password.length() >= MAX && uppercaseCounter >= MIN_Uppercase && lowercaseCounter >= MIN_Lowercase && digitCounter >= NUM_Digits) {
+                System.out.println("Valid Password");
+                return true;
+            } else {
+                System.out.println(" Su contraseña no contiene lo siguiente:");
+                if (password.length() < MAX)
+                    System.out.println(" [ Al menos 8 carácteres ]");
+                if (lowercaseCounter < MIN_Lowercase)
+                    System.out.println(" [ Letras minúsculas ]");
+                if (uppercaseCounter < MIN_Uppercase)
+                    System.out.println(" [ Letras mayúsculas ]");
+                if (digitCounter < NUM_Digits)
+                    System.out.println(" [ Número mínimo de dígitos numéricos ]");
+
+                return false;
+            }
         } else {
-            System.out.println(" Su contraseña no contiene lo siguiente:");
-            if (password.length() < MAX)
-                System.out.println(" [ Al menos 8 carácteres ]");
-            if (lowercaseCounter < MIN_Lowercase)
-                System.out.println(" [ Letras minúsculas ]");
-            if (uppercaseCounter < MIN_Uppercase)
-                System.out.println(" [ Letras mayúsculas ]");
-            if (digitCounter < NUM_Digits)
-                System.out.println(" [ Número mínimo de dígitos numéricos ]");
-
             return false;
         }
     }
 
     private boolean validEmail(String email) {
-        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        Matcher mather = pattern.matcher(email);
+        if (email != null) {
+            Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+            Matcher mather = pattern.matcher(email);
 
-        if (mather.find()) {
-            System.out.println("El email ingresado es válido.");
-            return true;
+            if (mather.find()) {
+                System.out.println("El email ingresado es válido.");
+                return true;
+            } else {
+                System.out.println("El email ingresado es inválido.");
+                return false;
+            }
         } else {
-            System.out.println("El email ingresado es inválido.");
             return false;
         }
     }
+
+    private boolean validDni(String dni) {
+        int i, j = 0;
+        String numero = ""; // Es el numero que se comprueba uno a uno por si hay alguna letra entre los 8 primeros digitos
+        String[] unoNueve = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        for (i = 0; i < dni.length() - 1; i++) {
+            numero = dni.substring(i, i + 1);
+
+            for (j = 0; j < unoNueve.length; j++) {
+                if (numero.equals(unoNueve[j])) {
+                    numero += unoNueve[j];
+                }
+            }
+        }
+
+        if (dni.length() != 8) {
+            System.out.println("El DNI ingresado es invalido.");
+            return false;
+        } else {
+            System.out.println("El DNI ingresado es valido.");
+            return true;
+        }
+    }
+
+    public static boolean validUsername(String name) {
+        String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
+        boolean specialchar = false;
+        boolean numero = false;
+        if (name.length() > 4 && name.length() < 15) {
+            for (int i = 0; i < name.length(); i++) {
+                if (specialChars.contains(String.valueOf(name.charAt(i)))) {
+                    specialchar = true;
+                } else if (Character.isDigit(Integer.valueOf(name.charAt(i)))) {
+                    numero = true;
+                }
+            }
+
+            if (specialchar && numero) {
+                System.out.println("El nombre " + name + " es invalido");
+                return false;
+            } else {
+                System.out.println("El nombre " + name + " es valido");
+                return true;
+            }
+        } else {
+            System.out.println("El nombre " + name + " no tiene caracteres suficientes.");
+            return false;
+        }
+    }
+
+
+//-----------------Opciones de Menu------------------//
+
+//---------Menu de prueba sin desarrollo------------//
+
+    private void MenuLogin() {
+        System.out.println("   [ Blockchain - V1 ]");
+        System.out.println(" 1 - Iniciar seccion.");
+        System.out.println(" 2 - Registrarse.. ");
+    }
+
+    private void MenuPrincipal(String nombreUser) {
+        System.out.println("      [ Blockchain - V1 ]");
+        System.out.println(" [ Bienvenido "+nombreUser+" ]");
+        System.out.println(" 1 - Deposito");
+        System.out.println(" 2 - Transferencia");
+        System.out.println(" 3 - Exit");
+    }
+
+
+
+
+    /////
+
+
+    public void tercero() {
+        Usuario uno = new Usuario("Alan", "alan@gmail", "password", "41079103", new Wallet());
+        Usuario dos = new Usuario("Nico", "nico@gmail", "password2", "31079103", new Wallet());
+        Usuario tres = new Usuario("Santi", "santi@gmail", "password3", "41459103", new Wallet());
+        Usuario cuatro = new Usuario("Masi", "masi@gmail", "password4", "41459103", new Wallet());
+
+
+        System.out.println(" Prueba N°1: Mostrar el mapa");
+        mapWallet.put(uno.getWallet().getCodigoSeguridad(), uno.getWallet().getUtnCoin());
+        mapWallet.put(dos.getWallet().getCodigoSeguridad(), dos.getWallet().getUtnCoin());
+        mapWallet.put(tres.getWallet().getCodigoSeguridad(), tres.getWallet().getUtnCoin());
+        mapWallet.put(cuatro.getWallet().getCodigoSeguridad(), cuatro.getWallet().getUtnCoin());
+
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("Muestro el usuario al que vamos a Transferir en el ejemplo siguiente: ");
+        System.out.println(cuatro.toString());
+        System.out.println(cuatro.getWallet().toString());
+        System.out.println("-----------------------------------------------------------------------------------");
+        int i = 0;
+
+        for (Map.Entry wallet : mapWallet.entrySet()) {
+            System.out.println(" Mapa N°" + i + ": " + wallet.toString());
+            i++;
+        }
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        i = 0;
+        for (Map.Entry wallet : mapWallet.entrySet()) {
+            System.out.println(" Mapa N°" + i + ": " + wallet.toString());
+            ///Si wallet es igual a el usuario cuatro, entonces ingresa.
+            if (wallet.getKey().equals(cuatro.getWallet().getCodigoSeguridad())) {
+                ///Todos los sout son para entender la logica/estructura del tp
+                ///Lo hice de esta manera porque me parecio mas logico que un usuario tenga un atributo billetera en vez de un atributo transferencias.
+                ///las transferencias se generan en la clase transferencias y se guardan en la BlockChain en un hashmap<key=User,Valor=transferencia>
+
+                System.out.println("Soy el usuario " + tres.getNombre() + " hoy tengo que pagar una cuota de $50. *logea la cuenta* ");
+                System.out.println("*Sistema* Genera Menu de opciones...");
+                System.out.println("*Usuario* elige la opcion Transferir");
+                System.out.println("*Clase transferir* Ingresa el NroCuenta a la que vas a tranferir: ");
+                System.out.println("*Usuario* ingresa: 1234");
+                System.out.println("*Sistema* Nro de Cuenta encontrada con exito.");
+                ///Crear funcion para retornar el USUARIO o el UUID para la funcion transferir.
+
+                System.out.println("*Menu Transferencia* " + tres.getNombre() + " ya podes ingresar el monto a transferir: (Scanner) ");
+                BigDecimal cuota = new BigDecimal(50);
+                System.out.println("*Usuario* : 50 ");
+                System.out.println("*Sistema* Confirmar transaccion (Si/No): *Usuario* Si.");
+                boolean respuesta = tres.getWallet().transferencia(tres.getWallet(), cuatro.getWallet(), cuota);
+                mapWallet.replace(cuatro.getWallet().getCodigoSeguridad(), cuatro.getWallet().getUtnCoin(), cuatro.getWallet().getUtnCoin().add(cuota));
+                System.out.println(wallet.toString());
+                System.out.println("La transferencia es: " + respuesta);
+                /// *Clase transferencia* Genera boleta y la guarda en BlockChain, la clase BlockChain tiene las funciones que son para buscar y mostrar.  nadie tiene acceso, no se puede borrar ni editar
+            }
+            i++;
+        }
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        i = 0;
+        for (Map.Entry wallet : mapWallet.entrySet()) {
+            System.out.println(" Mapa N°" + i + ": " + wallet.toString());
+            i++;
+        }
+    }
+
 
 
 }
--- src/com/company/Wallet.java
+++ src/com/company/Wallet.java
@@ -1,4 +1,106 @@
 package com.company;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class Wallet {
+
+
+    private UUID codigoSeguridad;
+    private BigDecimal utnCoin=new BigDecimal(100);
+    // Se usa BigDecimal para lo que son operaciones financieras.
+    // Es algo a implementar en proyectos relacionados a cuestiones financieras o blockchain.
+    // Garantiza más precision, asi disminuimos la posibilidad de perder decimales.
+
+    ///Basic
+    public Wallet(UUID codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+
+    ///Auto
+    public Wallet(){
+        this.codigoSeguridad=UUID.randomUUID();
+    }
+
+    public UUID getCodigoSeguridad() {
+        return codigoSeguridad;
+    }
+
+    public void setCodigoSeguridad(UUID codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+
+    public BigDecimal getUtnCoin() {
+        return utnCoin;
+    }
+
+    public void setUtnCoin(BigDecimal utnCoin) {
+        this.utnCoin = utnCoin;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Wallet{" +
+                "codigoSeguridad=" + codigoSeguridad +
+                ", utnCoin=" + utnCoin +
+                '}';
+    }
+
+    public boolean ingreso(Wallet receptor,BigDecimal ingreso) {
+        boolean ingresoCorrecto = true;
+        try {
+            if (ingreso.intValue() > 0) {
+                ingresoCorrecto = false;
+            } else {
+                receptor.setUtnCoin(receptor.getUtnCoin().add(ingreso));
+            }
+        }catch (ArithmeticException arithmeticException)
+        {
+            System.out.println("Error aritmetico en el ingreso de utnCoin$");
+        }
+        return ingresoCorrecto;
+    }
+
+    //método reintegro
+    public boolean reintegro(Wallet emisor,BigDecimal n) {
+        boolean reintegroCorrecto = true;
+        try {
+            if (n.intValue() < 0) {
+                reintegroCorrecto = false;
+            } else if (emisor.getUtnCoin().equals(n)) {
+                emisor.setUtnCoin(emisor.getUtnCoin().subtract(n));
+            } else {
+                reintegroCorrecto = false;
+            }
+        }catch(ArithmeticException arithmeticException)
+        {
+            System.out.println("Error aritmetico en el ingreso de utnCoin$");
+        }
+        return reintegroCorrecto;
+    }
+
+    public boolean transferencia(Wallet emisor, Wallet receptor, BigDecimal n) {
+        boolean correcto = true;
+        try{
+            if (n.intValue() < 0) {
+                correcto = false;
+            } else if (emisor.getUtnCoin().intValue()>=n.intValue()) {
+                emisor.reintegro(emisor,n);
+                emisor.ingreso(receptor,n);
+            } else {
+                correcto = false;
+            }
+        }catch (ArithmeticException arithmeticException)
+        {
+            System.out.println("Error aritmetico en el ingreso de utnCoin$");
+        }
+        return correcto;
+    }
+
+
 }
+
+
+
+
